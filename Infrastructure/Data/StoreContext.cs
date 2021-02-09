2cc20b150f84d9a159e1f426f37ca173afb8894f
Andre <andre@krooze.com.br>
2021-02-09 20:57:42 -0300

    Initial commit

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        { 
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
